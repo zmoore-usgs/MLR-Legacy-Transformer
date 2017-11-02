cb6f9cc7c46217dbf7e02580906285aac3f5ebd9
Mary Bucknell <mbucknell@usgs.gov>
2017-11-02 12:57:28 -0500

    Added /version endpoint and updated CHANGELOG.md

--- services.py
+++ services.py
@@ -1,3 +1,4 @@
+import pkg_resources
 import re
 
 from flask import request
@@ -15,30 +16,31 @@ api = Api(application,
           doc='/api')
 
 lat_lon_model = api.model('LatLonModel', {
-    'latitude' : fields.String,
-    'longitude' : fields.String,
-    'coordinateDatumCode' : fields.String,
+    'latitude': fields.String,
+    'longitude': fields.String,
+    'coordinateDatumCode': fields.String,
 })
 decimal_lat_lon_model = api.model('DecimalLatLonModel', {
-    'decimalLatitude' : fields.Float,
-    'decimalLongitude' : fields.Float
+    'decimalLatitude': fields.Float,
+    'decimalLongitude': fields.Float
 })
 station_name_model = api.model('StationNameModel', {
-    'stationName' : fields.String
+    'stationName': fields.String
 })
 station_ix_model = api.model('StationIxModel', {
-    'stationIx' : fields.String
+    'stationIx': fields.String
 })
 
 expected_lat_lon_model_keys = set(iter(lat_lon_model.keys()))
 expected_station_name_model_keys = set(iter(station_name_model.keys()))
 
+
 def _handle_missing_keys(json_data, expected_keys):
-    '''
+    """
     :param dict json_data:
     :param set expected_keys:
     :raises BadRequest if any keys in expected_keys are missing from json_data
-    '''
+    """
     request_keys = set(iter(json_data.keys()))
     missing_keys = expected_keys.difference(request_keys)
     if missing_keys:
@@ -69,4 +71,30 @@ class StationIx(Resource):
         request_body = request.get_json()
         _handle_missing_keys(request_body, expected_station_name_model_keys)
         station_ix = re.sub('\s|[^a-zA-Z0-9]', '', request_body.get('stationName'))
-        return  {'stationIx' : station_ix.upper()}, 200
+        return {'stationIx': station_ix.upper()}, 200
+
+
+version_model = api.model('VersionModel', {
+    'version': fields.String,
+    'artifact': fields.String
+})
+
+
+@api.route('/version')
+class Version(Resource):
+
+    @api.response(200, 'Success', version_model)
+    def get(self):
+        try:
+            distribution = pkg_resources.get_distribution('usgs_wma_mlr_legacy_transformer')
+        except pkg_resources.DistributionNotFound:
+            resp = {
+                "version": "local_development",
+                "artifact": None
+            }
+        else:
+            resp = {
+                "version": distribution.version,
+                "artifact": distribution.project_name
+            }
+        return resp

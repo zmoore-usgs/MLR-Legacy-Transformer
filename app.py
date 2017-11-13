5220952e6055d079d4b1eeedc0804371fed4b632
Mary Bucknell <mbucknell@usgs.gov>
2017-11-13 15:32:41 -0600

    Added new config variables to application for authentication and added requests dependency.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 from flask import Flask
 
@@ -6,9 +7,15 @@ application = Flask(__name__)
 
 application.config.from_object('config')
 
-PROJECT_DIR = PROJECT_DIR = os.path.dirname(__file__)
+PROJECT_DIR = os.path.dirname(__file__)
 if os.path.exists(os.path.join(PROJECT_DIR, '.env')):
     application.config.from_pyfile('.env')
+
+if application.config.get('AUTH_TOKEN_KEY_URL'):
+    resp = requests.get(application.config.get('AUTH_TOKEN_KEY_URL'), verify=application.config['AUTH_CERT_PATH'])
+    application.config['JWT_PUBLIC_KEY'] = resp.json()['value']
+    application.config['JWT_ALGORITHM'] = 'RS256'
+
 
 from services import *
 

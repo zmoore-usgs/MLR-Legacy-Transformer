24f7b8c524aed18f63282c95c2ed2350ff77284d
Zack Moore <zmoore@usgs.gov>
2018-11-19 14:10:33 -0600

    Added logging for lat/lon transformation errors.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from pyproj import Proj, transform
+from app import application
 
 nad83_proj = Proj('+init=EPSG:4269')
 nad27_proj = Proj('+init=EPSG:4267')
@@ -16,13 +17,13 @@ def transform_to_decimal_degrees(degrees, minutes, seconds):
 def transform_latitude_to_decimal_degrees(angle):
     result = transform_to_decimal_degrees(angle[0:3], angle[3:5], angle[5:])
     if result > 90.0 or result < -90.0:
-        raise ValueError
+        raise ValueError('Latitude angle ' + str(angle) + ' is not in the inclusive range of (-90, 90)')
     return result
 
 def transform_longitude_to_decimal_degrees(angle):
     result = -transform_to_decimal_degrees(angle[0:4], angle[4:6], angle[6:])
     if result > 180.0 or result < -180.0:
-        raise ValueError
+        raise ValueError('Longitude angle ' + str(angle) + ' is not in the inclusive range of (-180, 180)')
     return result
 
 def transform_location_to_decimal_location(latitude, longitude, coord_datum):
@@ -31,8 +32,10 @@ def transform_location_to_decimal_location(latitude, longitude, coord_datum):
         try:
             dlat = transform_latitude_to_decimal_degrees(latitude)
             dlon = transform_longitude_to_decimal_degrees(longitude)
-        except ValueError:
+        except ValueError as err:
             dlat, dlon = None, None
+            application.logger.error("An error occurred while transforming lat/lon to decimal degrees: "
+                + str(err))
         else:
             if datum != 'NAD83':
                 dlon, dlat = transform(DATUM_TO_PROJ[datum], nad83_proj, dlon, dlat)
